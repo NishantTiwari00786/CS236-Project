1858e39e34fca2ceb7652f523fbdee6f8d7ace9f
Nishant Tiwari <nishanttiwari@Nishants-MacBook-Pro-7.local>
2025-11-03 21:23:33 -0800

    Created database population still need to test it

--- scripts/phase2/database_population.py
+++ scripts/phase2/database_population.py
@@ -6,7 +6,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
     .appName("database-population") \
-    .config("spark.jars", "path/to/postgresql-42.7.3")\
+    .config("spark.jars", "/Users/nishanttiwari/Desktop/postgresql-42.7.8.jar")\
     .getOrCreate()
     
 # postgreSQL connection information 
@@ -27,4 +27,25 @@ hotel_df = spark.read.csv("data/clean/hotel-booking-cleaned.csv", header=True, i
 unified_df = spark.read.csv("data/unified/unified-dataset_fix.csv", header=True, inferSchema=True)
 
 
+# writing to postgres and overwriting if the table already exists
 
+customer_df.write.jdbc(jdbc_url, "customer_reservations", mode="overwrite", properties=connection_properties)
+hotel_df.write.jdbc(jdbc_url, "hotel_bookings", mode="overwrite", properties=connection_properties)
+unified_df.write.jdbc(jdbc_url, "unified_bookings", mode="overwrite", properties=connection_properties)
+
+
+# verifying the data has been written correctly by reading back from the database
+
+print("Data successfully written to PostgreSQL database.")
+
+print({"customer_reservations": customer_df.count(), "hotel_bookings": hotel_df.count(), "unified_bookings": unified_df.count()})
+
+spark.stop()
+
+
+
+
+
+
+
+# https://www.geeksforgeeks.org/python/pyspark-apply-custom-schema-to-a-dataframe/ used this for reference on applying schema
